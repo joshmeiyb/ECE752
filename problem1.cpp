57f1bb9c6aa3771f2d5da8fb98455633142972b2
Josh Mei <jmei@snares-02.cs.wisc.edu>
2023-04-02 00:09:02 -0500

    update

--- problem1.cpp
+++ problem1.cpp
@@ -1,130 +1,171 @@
 #include <cstdlib>
 #include <iostream>
+#include <random>
 #include <sys/time.h>
 
-
-#define ITER   10
-#define STEPS 64*1024*1024 //64MB, arbitraty number of steps
-#define MB    (1024*1024)
-
-// LLC Parameters assumed
-#define START_SIZE 1*MB
-#define STOP_SIZE  12*MB
-
+#define MB     1024*1024
+#define KB     1024
+#define B      1
 
 using namespace std;
 
-char array[STEPS];
+std::random_device rd;  //seed
+std::default_random_engine generator(rd()); //random number generator
 
-
-
-/////////////////////////////////////////////////////////
-// Provides elapsed Time between t1 and t2 in milli sec
-/////////////////////////////////////////////////////////
 
 double elapsedTime(timeval t1, timeval t2){
   double delta;
   delta = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
   delta += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
-  return delta; STEPS
+  return delta; 
 }
 
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
+void cacheAccess(int size){
 
-double DummyTest(void)
-{    
   timeval t1, t2;
   int ii, iterid;
-
-  // start timer
-  gettimeofday(&t1, NULL);
-
-  for(iterid=0;iterid<ITER;iterid++){
-    for(ii=0; ii< STEPS; ii++){
-      array[ii] += rand();
-    }
-  }
-
-  // stop timer
-  gettimeofday(&t2, NULL);
- 
-  return elapsedTime(t1,t2);
-}
-
-
-
-/////////////////////////////////////////////////////////
-// Change this, including input parameters
-/////////////////////////////////////////////////////////
-
-double CacheNumLevelsTest(void)
-{    
-  double retval;
-
-  return retval; 
-}
-
-
-/////////////////////////////////////////////////////////
-// Change this, including input parameters
-/////////////////////////////////////////////////////////
-
-double CacheSizeTest(int stride)
-{    
-  timeval t1, t2;
-
-  // start timer
-  gettimeofday(&t1, NULL);
-
-  for (int i = 0; i < STEPS; i = i + stride){ //use different stride in loop, strides shows cache size
-    array[(i*16) % sizeof(array)]++;
-  }
-
-  // stop timer
-  gettimeofday(&t2, NULL);
-
-  return elapsedTime(t1,t2); 
-}
-
-
-/////////////////////////////////////////////////////////
-// Change this, including input parameters
-/////////////////////////////////////////////////////////
-
-double LineSizeTest(void)
-{    
-  double retval;
-
-  return retval; 
-}
-
-
-/////////////////////////////////////////////////////////
-// Change this, including input parameters
-/////////////////////////////////////////////////////////
-
-double MemoryTimingTest(void)
-{    
-  double retval;
-
-  return retval; 
-}
-
-
-
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-
-int main(){
   
-  cout << "\nStarting DummyTest:" << "\n";
-  cout << "Test took " << DummyTest()/1000.0 << " seconds\n";
+  //Initialize buffer 
+  int buffer_size = size / B;           //cache unit size is Byte
+  char* buffer = new char[buffer_size]; //Generate a dynamic array to hold data
+  fill(buffer, buffer+buffer_size, 1);  //initialize buffer array with all 1s
 
-  // Add your code here, and comment above
+  std::uniform_int_distribution<int> distribution(0, buffer_size - 1);  //distribution on which to apply the generator
 
-  cout << "\n";
+  int access_time = 100000000;          //Make a large access number
+
+  vector<int> random_index;
+  for (int i; i < access_time; i++){
+    int index = distribution(generator);
+    random_index.push_back(index);      //push the element into a the random_index vector from the back
+  }
+
+  int total_data = 0;
+
+  // start timer
+  gettimeofday(&t1, NULL);
+  for(int i = 0; i < access_time; i++){
+    total_data += buffer[random_index[i]];
+  }
+  // stop timer
+  gettimeofday(&t2, NULL);
+
+  double time_span = elapsedTime(t1,t2);
+  int buffer_size_kb = buffer_size / B;
+  double throughput = double(total_data / B) / time_span; //throughput kbps
+
+  delete[] buffer;  //free memory
 }
 
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
+
+// /////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////
+
+// double DummyTest(void)
+// {    
+//   timeval t1, t2;
+//   int ii, iterid;
+
+//   // start timer
+//   gettimeofday(&t1, NULL);
+
+//   for(iterid=0;iterid<ITER;iterid++){
+//     for(ii=0; ii< STEPS; ii++){
+//       array[ii] += rand();
+//     }
+//   }
+
+//   // stop timer
+//   gettimeofday(&t2, NULL);
+ 
+//   return elapsedTime(t1,t2);
+// }
+
+
+
+// /////////////////////////////////////////////////////////
+// // Change this, including input parameters
+// /////////////////////////////////////////////////////////
+
+// double CacheNumLevelsTest(void)
+// {    
+//   double retval;
+
+//   return retval; 
+// }
+
+
+// /////////////////////////////////////////////////////////
+// // Change this, including input parameters
+// /////////////////////////////////////////////////////////
+
+// double CacheSizeTest(int stride)
+// {    
+  
+//   //TODO: 1. Create a array with the size of L1 cache ITER*size 
+//   //      2. Iterate over the buffer to load all data into cache
+//   //      3. 
+  
+  
+//   timeval t1, t2;
+
+//   for (int i = 0; i < ITER ; i++){
+//     // start timer
+//     gettimeofday(&t1, NULL);
+//     for (int j = 0; j < STEPS; j = j + (stride*Byte)){ //use different stride in loop, strides shows cache block size in each way
+//       array[(j * 16) % sizeof(array)]++;
+//     }
+//     // stop timer
+//     gettimeofday(&t2, NULL);
+//     printf("ITER num: %d, size cache: %d, stride: %d, access time: %f\n", i, STEPS, stride, elapsedTime(t1, t2));
+//   }
+
+//   return 0;
+// }
+
+
+// /////////////////////////////////////////////////////////
+// // Change this, including input parameters
+// /////////////////////////////////////////////////////////
+
+// double LineSizeTest(void)
+// {    
+//   double retval;
+
+//   return retval; 
+// }
+
+
+// /////////////////////////////////////////////////////////
+// // Change this, including input parameters
+// /////////////////////////////////////////////////////////
+
+// double MemoryTimingTest(void)
+// {    
+//   double retval;
+
+//   return retval; 
+// }
+
+
+
+// /////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////
+
+// int main(){
+  
+//   cout << "\nStarting DummyTest:" << "\n";
+//   cout << "Test took " << DummyTest()/1000.0 << " seconds\n";
+
+//   int stride = 64; //skip amount of memory before next access
+
+//   printf("Start CacheSizeTest\n");
+//   CacheSizeTest(stride);
+
+//   // Add your code here, and comment above
+
+//   cout << "\n";
+// }
+
+// /////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////
--- report1.md
+++ report1.md
@@ -2,3 +2,22 @@
 
 # Test machine - CSL machine
 Intel Core i5-11500 Processor, due to the website https://ark.intel.com/content/www/us/en/ark/products/212277/intel-core-i511500-processor-12m-cache-up-to-4-60-ghz.html, the LLC size is 12MB
+
+# Step
+1. Run the command to get the infomation of cache size: `getconf -a | grep CACHE`
+2. Results: 
+
+LEVEL1_ICACHE_SIZE                 32768
+LEVEL1_ICACHE_ASSOC                
+LEVEL1_ICACHE_LINESIZE             64
+LEVEL1_DCACHE_SIZE                 49152
+LEVEL1_DCACHE_ASSOC                12
+LEVEL1_DCACHE_LINESIZE             64
+LEVEL2_CACHE_SIZE                  524288
+LEVEL2_CACHE_ASSOC                 8
+LEVEL2_CACHE_LINESIZE              64
+LEVEL3_CACHE_SIZE                  12582912
+LEVEL3_CACHE_ASSOC                 16
+LEVEL3_CACHE_LINESIZE              64
+
+L1 Cache index size = 49152 / (12 * 64) = 64
